7468524bf4149bb47ae28006bd33787e091c1621
Jagjit <jbhati3@uic.edu>
2019-05-10 09:25:27 -0500

    Finishing touches on server

--- UNO/UnoServer/src/Game.java
+++ UNO/UnoServer/src/Game.java
@@ -6,15 +6,28 @@ public class Game
     private Deck deck;
     int numPlayers;
     ArrayList<Player> players;
+    int direction = 0; //clockwise
+    int turn = 0;
+    String color;
+    boolean turn_rejected;
+
+    String prevRank;
 
 
     public Game()
     {
+        players = new ArrayList<>();
         deck = new Deck();
         deck.shuffle();
         numPlayers = 0;
+        color = "null";
+        turn_rejected = false;
+        turn = 0;
+        prevRank = "";
 
     }
+
+
 
     public void addPlayer(int id)
     {
@@ -42,6 +55,86 @@ public class Game
         return serialized;
     }
 
+    public void draw(int id, int numCards)
+    {
+        players.get(id).addToHand(deck.deal(numCards));
+    }
+
+    public int getTurn()
+    {
+        return turn;
+    }
+
+
+    public int getNextPlayer()
+    {
+        if(turn == 0)
+        {
+            if(direction == 0) return 1;
+
+            else return 3;
+        }
+
+        else if(turn == 3)
+        {
+            if(direction == 0) return 0;
+
+            else return 2;
+        }
+
+        else if(direction == 0) return direction + 1;
+
+        else return direction - 1;
+    }
+
+    public int getNumCardsByID(int id)
+    {
+        return players.get(id).getHand().size();
+    }
+
+    public void play(int id, String color, String rank)
+    {
+        Card c = deck.getCard(color, rank);
+
+
+        turn_rejected = false;
+        if(color.equals("null")) color = c.color;
+
+        if(color.equals("null") || c.getColor().equals(color) || c.getRank().equals("w") || c.getRank().equals("w4") || c.getRank().equals(prevRank))
+        {
+            prevRank = c.getRank();
+            color = c.getColor();
+            if(c.getRank().equals("d2")) draw(getNextPlayer(), 2);
+
+            else if(c.getRank().equals("r")) {
+                if(direction == 0) direction = 1;
+
+                else direction = 0;
+            }
+
+            else if(c.getRank().equals("s")) {
+                turn = getNextPlayer();
+            }
+
+            else if(c.getRank().equals("w")) {
+                color = "null";
+            }
+
+            else if(c.getRank().equals("w4")) {
+                color = "null";
+                draw(getNextPlayer(), 4);
+            }
+
+            turn = getNextPlayer();
+        }
+
+        else turn_rejected = true;
+
+
+    }
+
+
+
     class Player
     {
         private ArrayList<Card> hand;
@@ -49,12 +142,15 @@ public class Game
 
         public Player(int id)
         {
+
             player_id = id;
+            hand = new ArrayList<>();
         }
 
         public void addToHand(ArrayList<Card> toDeal)
         {
             hand.addAll(toDeal);
+
         }
 
         public void draw(Card c)
@@ -81,12 +177,12 @@ public class Game
             String suit, rank;
             for(int i = 0; i<4; i++)
             {
-                for(int j = 0; j<25; i++)
+                for(int j = 0; j<25; j++)
                 {
-                    if(i == 0) suit = "Red";
-                    else if(i == 1) suit = "Green";
-                    else if(i == 2) suit = "Blue";
-                    else if(i == 3) suit = "Yellow";
+                    if(i == 0) suit = "red";
+                    else if(i == 1) suit = "green";
+                    else if(i == 2) suit = "blue";
+                    else if(i == 3) suit = "yellow";
                     else suit = "null";
 
                     if(j == 0) rank = "0";
@@ -111,9 +207,9 @@ public class Game
 
                     else if(j == 18 || j == 19) rank = "d2";
 
-                    else if(j == 20 || j == 21) rank = "s";
+                    else if(j == 20 || j == 21) rank = "skip";
 
-                    else if(j == 22 || j == 23) rank = "r";
+                    else if(j == 22 || j == 23) rank = "reverse";
 
 
                     else {
@@ -138,12 +234,20 @@ public class Game
             ArrayList<Card> dealing = new ArrayList<>();
             for(int i = 0; i<numCards; i++)
             {
-                dealing.add(cards.get(cards.size()));
-                dealing.remove(cards.size());
+                dealing.add(cards.get(cards.size() - 1));
+                cards.remove(cards.size() - 1);
             }
 
             return dealing;
         }
+
+        public Card getCard(String color, String rank)
+        {
+            return cards.get(cards.indexOf(new Card(color, rank)));
+
+        }
+
+
 
         public void shuffle()
         {
--- UNO/UnoServer/src/Server.java
+++ UNO/UnoServer/src/Server.java
@@ -12,6 +12,7 @@ public class Server
 
 
 
+
     public Server(int port)
     {
         try
@@ -68,6 +69,10 @@ public class Server
         private String input;
         private DataInputStream dis;
         private DataOutputStream dos;
+        private PrintWriter pw;
+        private Scanner sc;
+        boolean player1_ready, player2_ready, player3_ready, player4_ready;
+        private int ClientID;
 
 
 
@@ -81,8 +86,13 @@ public class Server
             {
                 dis = new DataInputStream(client.getInputStream());
                 dos = new DataOutputStream(client.getOutputStream());
-                clients.add(new Client(client, dis, dos));
+                pw = new PrintWriter(client.getOutputStream(), true);
+                sc = new Scanner(client.getInputStream());
+                clients.add(new Client(client, dis, dos, pw, sc));
                 game.addPlayer(clients.size());
+                ClientID = clients.size() - 1;
+                clients.get(clients.size() - 1).setID(ClientID);
+
             }
 
             catch (IOException e)
@@ -101,18 +111,29 @@ public class Server
                 System.out.println("connected");
                 String toWrite = "message;Welcome, player " + clients.size() + "! Waiting for other players to join....";
 
-                dos.writeUTF(toWrite);
+                //dos.writeUTF(toWrite);
+                System.out.println("player joined;" + clients.size());
+                pw.println("player joined;" + clients.size());
+
+
+                //dos.writeUTF("start game\n");
+
+               // pw.println("start game");
 
                 for(int i = 0; i<(clients.size() - 1); i++)
                 {
-                    clients.get(i).getDos().writeUTF("player joined;" + clients.size());
+                   // clients.get(i).getDos().writeUTF("player joined;" + clients.size());
+                    clients.get(i).getPw().println("player joined;" + clients.size());
+                    //(new PrintWriter(clients.get(i).getDos())).println("player joined;" + clients.size());
                 }
 
                 if(clients.size() == 4)
                 {
                     for(int i = 0; i<clients.size(); i++)
                     {
-                        clients.get(i).getDos().writeUTF("start game");
+                        clients.get(i).getPw().println("start game");
+
+
                     }
                 }
 
@@ -127,10 +148,52 @@ public class Server
 
                     if(command.equals("hand"))
                     {
-                        String toRet = "send hand;" + game.returnSerializedHand(clients.size() - 1);
+                        if(ClientID == 0) player1_ready = true;
+                        else if(ClientID ==1) player2_ready = true;
+                        else if(ClientID == 2) player3_ready = true;
+                        else if(ClientID == 3) player4_ready = true;
 
+                        if(player1_ready && player2_ready && player3_ready && player4_ready)
+                        {
+                            for(int i = 0; i<clients.size(); i++)
+                            {
+                                if(clients.get(i).getID() == game.getTurn()) clients.get(i).getPw().println("your turn");
+                                break;
+                            }
+                        }
                         //game.dealTo(clients.size() - 1);
                     }
+
+                    if(command.equals("turn"))
+                    {
+                        if(ClientID == game.getTurn())
+                        {
+                            if(params[0].equals("play"))
+                            {
+
+
+
+                                game.play(ClientID, params[1], params[2]);
+                            }
+
+                            else
+                            {
+
+                                game.draw(ClientID, 1);
+                            }
+
+                            clients.get(ClientID).getPw().println("send hand;" + game.returnSerializedHand(ClientID));
+                        }
+
+                        else clients.get(ClientID).getPw().println("message;Wait...It's not your turn!");
+
+                        for(int i = 0; i<clients.size(); i++)
+                        {
+
+                            if(ClientID != clients.get(i).getID()) clients.get(i).getPw().println("all cards;" + game.getNumCardsByID(ClientID));
+                        }
+                    }
+
                 }
             }
 
@@ -150,15 +213,21 @@ public class Server
 
     class Client
     {
+        private int id;
         private Socket sock;
         private DataInputStream dis;
         private DataOutputStream dos;
+        private PrintWriter pw;
+        private Scanner sc;
 
-        public Client(Socket sock, DataInputStream dis, DataOutputStream dos)
+        public Client(Socket sock, DataInputStream dis, DataOutputStream dos, PrintWriter pw, Scanner sc)
         {
             this.sock = sock;
             this.dis = dis;
             this.dos = dos;
+            this.pw = pw;
+            this.sc = sc;
+
         }
 
         public Socket getSock()
@@ -175,6 +244,20 @@ public class Server
         {
             return dos;
         }
+
+        public void setID(int id)
+        {
+            this.id = id;
+        }
+
+        public int getID()
+        {
+            return id;
+        }
+
+
+
+        public PrintWriter getPw() {return pw;}
     }
 
 
