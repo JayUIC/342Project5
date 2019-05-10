a245b582ad23dec81838ac390fec18e5e19e63c8
nbezin2 <47515136+nbezin2@users.noreply.github.com>
2019-05-10 05:41:51 -0500

    Update Client.java
    Added: send hand op code, Your turn op code, messages op code, all cards op code, worked on the in game GUI and created an array list of Card objects

--- UNO/UnoClient/src/sample/Client.java
+++ UNO/UnoClient/src/sample/Client.java
@@ -20,10 +20,14 @@ import javafx.scene.layout.StackPane;
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Client {
 
     private Socket serverConnection;
+    private ArrayList<Card> hand = new ArrayList<>();
+    private String[] numCards = {"0", "0", "0", "0"};
+
 
 
     private Thread listener = new serverComm();
@@ -31,7 +35,7 @@ public class Client {
     private PrintWriter out;
     private Scanner in;
 
-    private String numPlayersIn;
+    private String playerID = "0";
 
 
     Stage mainStage = new Stage();
@@ -43,6 +47,12 @@ public class Client {
     HBox cardTable = new HBox();
     HBox gameTable = new HBox();
 
+    Label p1 = new Label("");
+    Label p2 = new Label("");
+    Label p3 = new Label("");
+    Label p4 = new Label("");
+
+
     Client(Socket s){
         serverConnection = s;
 
@@ -53,12 +63,12 @@ public class Client {
         catch (IOException e){
 
         }
-
     }
 
 
 
     public void loadLobby() {
+
         Pane lobbyPane = new Pane();
         ScrollPane lobbyContainer = new ScrollPane();
         Label lobbyTitle = new Label("Lobby");
@@ -88,6 +98,20 @@ public class Client {
         lobbyContainer.setContent(lobbyPane);
     }
 
+    public void all_cards(String[] numberOfCards) {
+        p1.setText("Number of Cards Player 1 has: " + numberOfCards[1]);
+        p2.setText("Number of Cards Player 2 has: " + numberOfCards[2]);
+        p3.setText("Number of Cards Player 3 has: " + numberOfCards[3]);
+        p4.setText("Number of Cards Player 4 has: " + numberOfCards[4]);
+    }
+
+    public void message(String message) {
+        consoleMessage.setText(message);
+    }
+
+    public void your_turn() {
+        consoleMessage.setText("It's your turn!");
+    }
 
     public void player_joined(String playersCon) {
         if (playersCon.equals("1")) {
@@ -97,10 +121,40 @@ public class Client {
             lobbyMessage.setText("There are " + playersCon + " players connected to the server.");
         }
     }
+
+    public void send_hand(String[] cards) {
+        hand.clear();
+        String delim = ",";
+        for(int i = 1; i < cards.length; i++) {
+            String[] cardElements = cards[i].split(delim);
+            Card newCard = new Card(cardElements[0], cardElements[1]);
+            hand.add(newCard);
+        }
+    }
+
+    public void playerCardMessages() {
+        p1.setFont(Font.font("Arial",FontWeight.BOLD,14));
+        p1.setTextFill(Color.BLACK);
+
+        p2.setFont(Font.font("Arial",FontWeight.BOLD,14));
+        p2.setTextFill(Color.BLACK);
+
+        p3.setFont(Font.font("Arial",FontWeight.BOLD,14));
+        p3.setTextFill(Color.BLACK);
+
+        p4.setFont(Font.font("Arial",FontWeight.BOLD,14));
+        p4.setTextFill(Color.BLACK);
+
+        String[] nums = {"0","0","0","0","0"};//////////////////////////////////////////////////////////////////////////
+        all_cards(nums);
+        p1.relocate(20, 20);
+        p2.relocate(20, 40);
+        p3.relocate(20, 60);
+        p4.relocate(20, 80);
+    }
     public void createGameConsoleGUI(){
         consoleMessage.setFont(Font.font("Arial", FontWeight.BOLD,18));
         consoleMessage.setTextFill(Color.LIGHTGREEN);
-
 
         gameConsole.relocate(225,450);
         gameConsole.setMinHeight(75);
@@ -110,19 +164,47 @@ public class Client {
         gameConsole.setStyle("-fx-background-color: #000000");
         gameConsole.getChildren().add(consoleMessage);
 
-        Background.getChildren().add(gameConsole);
+        Background.getChildren().addAll(gameConsole, p1, p2, p3, p4);
     }
     public void loadGameScreen(){
         Background.setStyle("-fx-background-color: #532fef;");
         mainScene = new Scene(Background,750,750);
         mainStage.setScene(mainScene);
+        mainStage.show();
 
         createGameConsoleGUI();
+        playerCardMessages();
 
         gameTable.setStyle("-fx-background-color: #3710e8;");
         gameTable.relocate(50,250);
         gameTable.setMinSize(100,100);
         gameTable.setAlignment(Pos.CENTER_LEFT);
+    }
+
+    class Card {
+        private String name;
+        private String color;
+
+        Card(String name, String color) {
+            this.name = name;
+            this.color = color;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getColor() {
+            return color;
+        }
     }
 
 
@@ -148,10 +230,45 @@ public class Client {
                 });
             }
             else if (methodCode.equals("player joined")) {
+                if(playerID.equals("0")) {
+                    playerID = pieces[1];
+                }
                 Platform.runLater(new Runnable() {
                     @Override
                     public void run() {
                         player_joined(pieces[1]);
+                    }
+                });
+            }
+            else if (methodCode.equals("message")) {
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        message(pieces[1]);
+                    }
+                });
+            }
+            else if (methodCode.equals("your turn")) {
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        your_turn();
+                    }
+                });
+            }
+            else if (methodCode.equals("send hand")) {
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        send_hand(pieces);
+                    }
+                });
+            }
+            else if (methodCode.equals("all cards")) {
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        all_cards(pieces);
                     }
                 });
             }
